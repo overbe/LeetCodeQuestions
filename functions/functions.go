f92600c4a7fee66be37ef1a015712ad62eb3bfe3
perepelica.m <perepelica.m@novaposhta.ua>
2019-07-30 15:41:30 +0300

    added solution 344

--- functions/functions.go
+++ functions/functions.go
@@ -11,3 +11,15 @@ func IntArrayEquals(a []int, b []int) bool {
 	}
 	return true
 }
+
+func StringArrayEquals(a []string, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
